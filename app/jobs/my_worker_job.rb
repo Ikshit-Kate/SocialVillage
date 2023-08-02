783171dd52dead18d958b734c44bd583c90c0451
Ikshit Kate <ikshitkate@gmail.com>
2023-08-02 15:41:12 +0530

    sidekiq+1

--- app/jobs/my_worker_job.rb
+++ app/jobs/my_worker_job.rb
@@ -1,11 +1,7 @@
-# app/jobs/my_worker_job.rb
 class MyWorkerJob < ApplicationJob
     queue_as :default
   
     def perform(arg1, arg2)
-      # Some custom logic based on arg1 and arg2
-      # For example, sending an email notification
-  
       user = User.find_by(id: arg1)
       order = Order.find_by(id: arg2)
   
